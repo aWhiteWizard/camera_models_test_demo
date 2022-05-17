49c3bae93e70809f8a2b10344579f17325ae8796
chasestat <www.123518341@qq.com>
2022-05-17 20:32:02 +0800

    merge the picture

---
+++ include/opencv_orb_match.h
@@ -0,0 +1,11 @@
+#ifndef __OPENCV_ORB_MATCH
+#define __OPENCV_ORB_MATCH
+
+#include <iostream>
+#include "opencv2/opencv.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/features2d/features2d.hpp"
+#include "opencv2/imgproc/imgproc_c.h"
+#include "opencv2/core/core_c.h"
+#include "opencv2/core/types_c.h"
+#endif
--- src/opencv_orb_match.cpp
+++ src/opencv_orb_match.cpp
@@ -1,3 +1,7 @@
+/************************************************************
+ * WhiteWizard Designed in 2022.5.17
+ * *********************************************************/
+
 #include "opencv_orb_match.h"
 
 using namespace cv;
@@ -5,6 +9,10 @@ using namespace std;
 
 String img_left ="/home/lan/Dataset/TUM_VI/noApril_tum/mav0/cam0/data";
 String img_right ="/home/lan/Dataset/TUM_VI/noApril_tum/mav0/cam1/data";
+
+void linespace(Mat&image,float begin,float finish,int w1,Mat&mask);
+void generate_mask(Mat&img,Mat&mask);
+ 
 
 int main(int argc, char const *argv[])
 {
@@ -14,8 +22,10 @@ int main(int argc, char const *argv[])
     cv::glob(img_left,img_lefts);
     cv::glob(img_right,img_rights);
 
-
-
+/******************************************************
+ * 读取双目图像
+ * 
+*******************************************************/
     if(img_lefts.size() == 0)
         cout << "left cam no data inputs,Please Check" << endl;
     
@@ -49,7 +59,7 @@ int main(int argc, char const *argv[])
 
         Mat outimg1;
         drawKeypoints(lefts, Keypoint1, outimg1, Scalar::all(-1), DrawMatchesFlags::DEFAULT);
-        imwrite("ORB_features.png", outimg1);
+        // imwrite("ORB_features.png", outimg1);
 
         vector<DMatch> matches;
         t1 = chrono::steady_clock::now();
@@ -66,24 +76,145 @@ int main(int argc, char const *argv[])
         cout << "---MIN dist: " << min_dist << endl;
 
         std::vector<DMatch> good_matches;
+        std::vector<Point2f> pic_point_1;
+        std::vector<Point2f> pic_point_2;
         for (int i = 0; i < descriptor1.rows ; i++)
         {
-            if(matches[i].distance <= max(1.33*max_dist, 30.0))
-                good_matches.push_back(matches[i]);
+            if(matches[i].distance <= max(2*min_dist, 30.0))
+                {
+                    good_matches.push_back(matches[i]);
+                }
         }
-    cv::Mat img_match;
-    cv::Mat img_goodmatch;
 
-    drawMatches(lefts, Keypoint1, rights, Keypoint2, matches, img_match);
-    drawMatches(lefts, Keypoint1, rights, Keypoint2, good_matches, img_goodmatch);
+        for (int t = 0; t < good_matches.size(); t++)
+        {
+            pic_point_1.push_back(Keypoint1[good_matches[t].queryIdx].pt);
+            pic_point_2.push_back(Keypoint1[good_matches[t].queryIdx].pt); 
+        }
+        cout << "pic_point_push-back" << endl;
+        Mat hamming = findHomography(pic_point_1, pic_point_2, RANSAC);
 
-    imshow("matches", img_match);
-    imshow("good matcher", img_goodmatch);
+        int mix_high = max(lefts.rows, rights.rows);
+        int mix_width = lefts.cols + rights.cols;
+        Mat mix_pic_01 = Mat::zeros(Size(mix_width, mix_high),CV_8UC1);
+        cv::Rect roi;
+        roi.x = 0;
+        roi.y = 0;
+        roi.width = lefts.cols;
+        roi.height = lefts.rows;
+
+        cout << "copy to pic" << endl;
+        //对齐图像
+        lefts.copyTo(mix_pic_01(roi));
+        // imshow("mix_pic_01", mix_pic_01);
+
+        cv::Mat mix_pic_02;
+        warpPerspective(rights, mix_pic_02, hamming, Size(mix_width, mix_high));
+        // imshow("mix_pic_02", mix_pic_02);
+        cout << "mask creative" << endl;
+        //生成mask
+        cv::Mat mask = cv::Mat::zeros(Size(mix_width, mix_high), CV_8UC1);
+        generate_mask(mix_pic_02,mask);
+        // imshow("mask", mask);
+        cout << "mask diff" << endl;
+        //初始化mask权重
+        cv::Mat mask1 = cv::Mat::ones(Size(mix_width, mix_high), CV_8UC1);
+        cv::Mat mask2 = cv::Mat::ones(Size(mix_width, mix_high), CV_8UC1);
+        cout << "left & right mask" << endl;
+        //left mask1
+        linespace(mask1, 1, 0, lefts.cols, mask);
+        // imshow("mask1", mask1);
+        //right mask2
+        linespace(mask2, 1, 0, rights.cols, mask);
+        // imshow("mask2", mask2);
+        cout << "left multiply" << endl;
+                
+        Mat m_left;
+        std::vector<Mat> mv;
+        mv.push_back(mask1);
+        mv.push_back(mask1);
+        mv.push_back(mask1);
+        cout << "left merge" << endl;
+        merge(mv,m_left);
+        cout << "left convertTo" << endl;
+        mix_pic_01.convertTo(mix_pic_01, CV_8U);
+        cout << "left multiply_real" << endl;
+        cv::multiply(mix_pic_01, m_left, mix_pic_01);
+        
+        cout << "right multiply" << endl;    
+        Mat m_right;
+        mv.push_back(mask2);
+        mv.push_back(mask2);
+        mv.push_back(mask2);
+        merge(mv,m_right);
+        mix_pic_02.convertTo(mix_pic_02, CV_8U);
+        multiply(mix_pic_02, m_right, mix_pic_02);
+        cout << "merge the pic" << endl;
+        //merge
+        cv::Mat mix_pic;
+        add(mix_pic_01, mix_pic_02, mix_pic);
+        mix_pic.convertTo(mix_pic, CV_8U);
+        imshow("mix_pic", mix_pic);
+        imwrite("./merge/" + std::to_string(frame) + "miximage.jpg", mix_pic);
+
+
+    // cv::Mat img_match;
+    // cv::Mat img_goodmatch;
+
+    // drawMatches(lefts, Keypoint1, rights, Keypoint2, matches, img_match);
+    // drawMatches(lefts, Keypoint1, rights, Keypoint2, good_matches, img_goodmatch);
+
+    // imshow("matches", img_match);
+    // imshow("good matcher", img_goodmatch);
     
-    imwrite("./orb_match/"+std::to_string(frame)+"match.jpg", img_match);
-    imwrite("./orb_match/"+std::to_string(frame)+"good_match.jpg", img_goodmatch);
+    // imwrite("./orb_match/"+std::to_string(frame)+"match.jpg", img_match);
+    // imwrite("./orb_match/"+std::to_string(frame)+"good_match.jpg", img_goodmatch);
     }
     waitKey(0);
     return 0;
 
+}
+/*******************************************************************
+ * 生成mask的函数
+ * ***************************************************************/
+void generate_mask(Mat&img, Mat&mask) {
+	int w = img.cols;
+	int h = img.rows;
+	for (int row = 0; row < h;row++) {
+		for (int col = 0; col < w; col++) {
+			Vec3b p = img.at<Vec3b>(row,col);
+			int b = p[0];
+			int g = p[1];
+			int r = p[2];
+			if (b == g && g == r && r == 0) {
+				mask.at<uchar>(row, col) = 255;
+			}
+ 
+		}
+		
+	}
+}
+
+void linespace(Mat&image, float begin, float finish, int w1, Mat&mask) {
+	int offstx = 0;
+	float interval = 0;
+	float delta = 0;
+	for (int i = 0; i < image.rows; i++) {
+		offstx = 0;
+		interval = 0;
+		delta = 0;
+		for (int j = 0; j < image.cols; j++) {
+			int pv = mask.at<uchar>(i, j);
+			if (pv == 0 && offstx == 0) {
+				offstx = j;
+				delta = w1 - offstx;
+				interval = (finish-begin) / (delta - 1);
+				image.at<float>(i, j) = begin + (j - offstx)*interval;
+			}
+			else if(pv==0&&offstx>0&&(j-offstx)<delta){
+				image.at<float>(i, j) = begin + (j - offstx)*interval;
+			}
+			 
+		}
+	}
 }
